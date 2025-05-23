0c165664b9db3446ddc7e500040d7698d184cbad
abbaszal <zal.abbasl76@gamil.com>
2025-05-23 23:51:45 +0200

    Update

--- src/HuGaDB_with_LQC.py
+++ src/HuGaDB_with_LQC.py
@@ -14,6 +14,7 @@ warnings.filterwarnings("ignore", category=ConvergenceWarning)
 warnings.filterwarnings("ignore", message="X does not have valid feature names")
 
 
+
 #local imports
 from utils.HuGaDB.prepare_partitions import prepare_partitions
 from utils.aggregate_functions import aggregate_lr_models,FederatedForest
@@ -109,7 +110,7 @@ def run_experiment(approach,n_trials,n_clients,hyper_params,noise_std,num_corrup
     train_pattern=os.path.join(data_root,'train_{:02d}.csv')
     test_pattern=os.path.join(data_root,'test_{:02d}.csv')
     #corruption_params={'corruption_prob':0.8,'nan_prob':0.5,'noise_std':noise_std}
-    #label_corruption_prob=0.1
+    #label_corruption_prob=0.2
     os.makedirs(save_dir,exist_ok=True)
     all_details=[]
     corrupt_client_indices=list(range(num_corrupted_clients))
@@ -118,28 +119,52 @@ def run_experiment(approach,n_trials,n_clients,hyper_params,noise_std,num_corrup
     for hyper_param in hyper_params:
 
         details_for_this_param=[]
+        client_accuracy_details = []
 
         for trial in range(n_trials):
-
-            rand_component=random.randint(0,500)
-            trial_seed=base_random_seed+trial+int(1000*hyper_param)+rand_component
+            rand_component = random.randint(0,500)
+            trial_seed = base_random_seed+trial+int(1000*hyper_param)+rand_component
             random.seed(trial_seed);np.random.seed(trial_seed)
 
             df_results,df_nash,client_global_accuracies=run_trial(
                 approach,n_clients,trial_seed,sample_size,num_corrupted_clients,
                 train_pattern,corruption_settings,hyper_param,noise_std,
                 aggregator_func,X_test_global_scaled,y_test_global,corrupt_client_indices)
-            
-            df_nash['Trial']=trial+1;df_nash['Noise Std']=noise_std
-            df_nash['Corrupted Clients']=num_corrupted_clients;df_nash['Max Iter or Depth']=hyper_param\
-            
+
+            df_nash['Trial'] = trial + 1
+            df_nash['Noise Std'] = noise_std
+            df_nash['Corrupted Clients'] = num_corrupted_clients
+            df_nash['Max Iter or Depth'] = hyper_param
             details_for_this_param.append(df_nash)
 
-        all_details.append(pd.concat(details_for_this_param,ignore_index=True))
+            trial_acc = {
+                'Trial': trial + 1,
+                'Max Iter or Depth': hyper_param,
+                'Noise Std': noise_std,
+                'Corrupted Clients': num_corrupted_clients
+            }
+            for j in range(n_clients):
+                col_name = f'Client {j+1} Accuracy'
+                if j in corrupt_client_indices:
+                    col_name += "(low-quality client)"
+                trial_acc[col_name] = client_global_accuracies[j] if client_global_accuracies[j] is not None else np.nan
+            client_accuracy_details.append(trial_acc)
 
-    final_details_df=pd.concat(all_details,ignore_index=True)
-    out_path=os.path.join(save_dir,f'Nash_Equilibrium_Details_{approach}_noise_{noise_std}_c{num_corrupted_clients}.csv')
-    final_details_df.to_csv(out_path,index=False);return final_details_df
+        df_details = pd.concat(details_for_this_param, ignore_index=True)
+        df_client_accuracy = pd.DataFrame(client_accuracy_details)
+        df_combined = df_details.merge(
+            df_client_accuracy,
+            on=['Trial', 'Max Iter or Depth', 'Noise Std', 'Corrupted Clients'],
+            how='left'
+        )
+        all_details.append(df_combined)
+
+    final_details_df = pd.concat(all_details, ignore_index=True)
+    out_path = os.path.join(save_dir, f'Nash_Equilibrium_Details_{approach}_noise_{noise_std}_c{num_corrupted_clients}.csv')
+    final_details_df.to_csv(out_path, index=False)
+
+    return final_details_df
+
 
 
 
